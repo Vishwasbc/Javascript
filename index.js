e96568223c01d73f90410241bf933493963f96d1
Vishwas <vishwasnayakbc@gmail.com>
2025-11-26 15:28:21 +0530

    DOM Manipulation

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log('Hello World');
 console.log('This is a simple Node.js application.');
+
+document.getElementById('name').innerText = 'Hello'
+document.getElementById('changeTextBtn').onclick = function () {
+    document.getElementById('name').innerText = 'Button Clicked';
+    document.getElementById('name').style.backgroundColor = 'red';
+}
+document.getElementById('changeTextBtn').ondblclick = function () {
+    document.getElementById('name').innerText = 'Button Clicked Twice';
+    document.getElementById('name').style.backgroundColor = 'white';
+};
