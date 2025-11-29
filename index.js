a13586a57e0ffb51e3bbc86a3c42c540c13efce9
Vishwas <vishwasnayakbc@gmail.com>
2025-11-29 12:01:27 +0530

    Vars in JS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+//Hello World
 console.log('Hello World');
 console.log('This is a simple Node.js application.');
-
+//Dom Manipulation
 document.getElementById('name').innerText = 'Hello'
 document.getElementById('changeTextBtn').onclick = function () {
     document.getElementById('name').innerText = 'Button Clicked';
@@ -10,3 +11,18 @@ document.getElementById('changeTextBtn').ondblclick = function () {
     document.getElementById('name').innerText = 'Button Clicked Twice';
     document.getElementById('name').style.backgroundColor = 'white';
 };
+//Variables
+let count=0;
+document.getElementById('count').innerText = count;
+document.getElementById('countBtn').onclick = function () {
+    count = count + 1;
+    document.getElementById('count').innerText = count;
+}
+document.getElementById('countBtnDec').onclick = function () {
+    count = count - 1;
+    document.getElementById('count').innerText = count;
+}
+document.getElementById('reset').onclick = function () {
+    count = 0;
+    document.getElementById('count').innerText = count;
+}
